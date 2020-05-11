284f0991ba62a8c1e27c1e3ad674e159e8a9119c
jessesongok <jessesongok@gmail.com>
2020-05-11 19:28:18 +0300

    Laravel UI Bootstrap Setup -> Installed Laravel/ui dependency -> Scaffolded the Laravel Auth Module

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |sudo a2enmod rewrite
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/hello', function (){
-    $word = ["message" => "Hello World"];
-    return json_encode($word);
-});
+Route::post('/login', 'API\UserController@login');
+Route::post('/register', 'API\UserController@register');
+Route::get('/logout', 'API\UserController@logout');
